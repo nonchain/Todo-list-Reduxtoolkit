afb81c2379e183f1763639ed21c63a7fd420abda
progali007 <code.progali@gmail.com>
2022-11-08 23:37:56 +0330

    Add error message

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 
 /*
 **************************************************
@@ -9,21 +10,23 @@ import { useFormContext } from 'react-hook-form';
 **************************************************
 */
 
-function Input({ type, id, label, name, length,...rest }) {
-   const { register } = useFormContext();
-   
+function Input({ id, label, name, length,...rest }) {
+   const { register, formState: { errors } } = useFormContext();
+
    return (
       <div className='flex flex-col gap-1'>
          <label className='text-base font-bold text-gray-600' htmlFor={id}>{label}</label>
          <input
             className='p-3 w-full text-gray-900 font-semibold border-2 border-gray-400 rounded duration-150 transition focus:border-gray-700 outline-none'
-            type={type}
+
             id={id}
             {...rest} 
             {...register(name , {
-               required: { value: true, message: `the ${name} field can not be blank` },
-               maxLength: { value: length, message: `Title is more than ${length} characters` },
+               required: { value: true, message: `The ${name} field can not be blank` },
+               maxLength: { value: length, message: `${name} is more than ${length} characters` },
             })}/>
+
+            {errors[name]?.message && <span className='text-sm text-red-500 font-semibold'>{errors[name]?.message}</span>}
       </div>
    )
 }
