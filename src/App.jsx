b17309a9cefd5e097569d69556e8a110c67d2a60
progali007 <code.progali@gmail.com>
2022-11-08 15:49:36 +0330

    Add Home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      Home page
+    <div className="px-5 py-8 min-h-screen bg-gray-100 flex items-center flex-col">      
+      <Home />
     </div>
   )
 }
