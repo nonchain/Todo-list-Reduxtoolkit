3c717c59b0388d3a965b21b25de9e15b21f50de1
progali007 <code.progali@gmail.com>
2022-11-09 13:17:42 +0330

    Add functionality

--- src/ui/CheckBox.jsx
+++ src/ui/CheckBox.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
-function CheckBox({ id }) {
+function CheckBox({ id, completedTask, setCompletedTask }) {
+   const checkedHandler = () => {
+      setCompletedTask(!completedTask);
+   }
    return (
       <label className='w-5 h-5 flex items-center gap-1 relative'>
-         <input type="checkbox" id={id} className='checkbox' />
+         <input type="checkbox" id={id} onChange={checkedHandler} className='checkbox' />
          <i className={`ri-check-line text-white absolute absolute-center hidden`}></i>
       </label>
    )
