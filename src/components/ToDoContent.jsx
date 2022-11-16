d64debc9d5cef5a40312d63be3e528161db251d8
progali007 <code.progali@gmail.com>
2022-11-16 13:27:01 +0330

    Show tasks based on category

--- src/components/ToDoContent.jsx
+++ src/components/ToDoContent.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAllTodo } from '../redux/features/todoSlice';
+import { selectAllTodo, selectFilterCategory } from '../redux/features/todoSlice';
 import ToDoItems from './ToDoItems';
 
 function ToDoContent() {
   const data = useSelector(selectAllTodo);
+  const category = useSelector(selectFilterCategory);
   const sortedData = [...data];
+  let filteredData = [];
   sortedData.sort((a, b) => new Date(b.time) - new Date(a.time))
 
+  if (category !== 'all') {
+    filteredData = sortedData.filter(todo => todo.category === category);
+  }
+  else {
+    filteredData = sortedData;
+  }
 
   return (
     <div className='w-full mt-6'>
       <ul className='grid gap-3'>
-        {sortedData?.map(item => <ToDoItems key={item.id} item={item}/>)}
+        {filteredData?.map(item => <ToDoItems key={item.id} item={item} />)}
       </ul>
     </div>
   )
