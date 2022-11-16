485951f624407c32dcc0df58a25c071ab233498c
progali007 <code.progali@gmail.com>
2022-11-16 15:45:05 +0330

    Responsive the design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {Toaster} from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div className="px-5 py-8 min-h-screen bg-gray-100 flex items-center flex-col">      
+    <div className="px-5 py-8 min-h-screen bg-gray-100 flex items-center flex-col md:px-8 md:py-10 lg:px-12 lg:py-16 xl:px-44">      
       <Home />
       <Toaster toastOptions={{
         style: {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import ToDoHeader from '../components/ToDoHeader'
 function Home() {
    return (
       <React.Fragment>
-         <h1 className='mb-8 text-2xl font-bold'>To-Do list</h1>
+         <h1 className='mb-8 text-2xl text-gray-800 font-bold lg:text-3xl xl:text-4xl xl:mb-10'>To-Do list</h1>
          <ToDoHeader />
          <ToDoContent />
       </React.Fragment>
